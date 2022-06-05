81b6c16cb90ad5c23646259048da47dd50b5e6ae
Creative-Difficulty <80907917+Creative-Difficulty@users.noreply.github.com>
2022-06-05 12:36:29 +0200

    simplifed code

--- index.js
+++ index.js
@@ -80,146 +80,140 @@ app.get("/" + process.env.PATH_AFTER_URL, function (req, res) {
             }
         }
     }
-    if(req.header("EucoAPIAuth") !== "IamRobot") {res.status(401).send("Error 401: Unauthorized"); return;}
+    if(req.header("EucoAPIAuth") !== "IamRobot") {
+        res.status(401).send({
+            "success": false,
+            "Error": "Unauthorized",
+            "ErrorCode": "401",
+            "ErrorMessage": "Invalid Headers" 
+        }); 
+        return;
+    }   
 
-    ReqCounter++;
-
-    var msTakenforTest = fib(30)
-
-    var isRaspi;
-    var piTemp
-    var piVoltage;
-    if(isPi()) {
-        isRaspi = true;
-        process2.exec("vcgencmd measure_temp",function (err,stdout,stderr) {stdout.split("="); var stdout1 = stdout[1].split("'"); piTemp = stdout1[0]})
-        process2.exec("vcgencmd measure_volts",function (err,stdout,stderr) {stdout.split("="); piVoltage = stdout[1].replace("V", "")})
-    }
-
-    process2.exec('whoami',function (err,stdout,stderr) { username = stdout.replace(/[\n\t\r]/g,"")})
-    var lel;
-    si.get({
-        osInfo: "*",
-        system: "*",
-        shell: "*",
-
-        audio: "*",
-
-        baseboard: "*",
-        chassis: "*",
-
-        cpu: "*",
-        //cpuFlags: "*",
-        cpuCache: "*", //in bytes
-        cpuCurrentSpeed: "*",
-        cpuTemperature: "*",
-
-        mem: "*", //in bytes
-        memLayout: "*", //in bytes
-
-        battery: "*",
-
-        graphics: "*",
-        versions: "*",
-        users: "*",
-        FullLoad: "*", //highest CPU % since boot
-
-        processes: "all, running, sleeping, unknown, blocked",
-        
-        //processLoad("process_name, process_name2"): "*", See: https://systeminformation.io/processes.html
-        services: "*",
-
-        diskLayout: "*",
-        fsOpenFiles: "*",
-        fsStats: "*", //See: https://systeminformation.io/filesystem.html
-
-        usb: "*",
-        printer: "*",
-
-        //networkInterfaces: "*",
-        networkInterfaceDefault: "*",
-        networkGatewayDefault: "*",  //router ip: 10.0.0.138
-        networkStats: "*", //si.networkStats(iface) iface= interface to scan
-        //networkConnections: "*",
-        //inetLatency: "*", inetLatency(google.com)
-
-        wifiNetworks: "*",
-        wifiInterfaces: "*",
-        wifiConnections: "*",
-
-        bluetoothDevices: "*",
-
-        dockerAll: "*",
-        vboxInfo: "*",
-    }).then(data => {lel = data; SendResponse();});
-
-    function SendResponse() {
-        si.osInfo().then(osStats => {osVersion = osStats})
-        mem.info().then(freemem => {freeMemory = freemem})
-        drive.info().then(driveinfo => {diskInfo = driveinfo})
-        cpu.usage().then(usage => {Processorusage = usage})
-        var cpuCoresInfo = os.cpus()
-        var cpuCoresInfo = cpuCoresInfo[1]
-        var data;
-        var today = new Date();
-        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     
-        data = {
-            "success" : true,
-            "time" : time,
-            "cpu_usage" : Processorusage,
-            "cpu_core_count" : cpu.count(),
-            "logged_in_user" : username,
-            "RAM" : freeMemory,
-            "cpu_type" : cpuCoresInfo,
-            "storage_info" : diskInfo,
-            "os_version" : osVersion,
-            "performance_test": msTakenforTest,
-            "Raspi": {
-                "is_pi": isRaspi,
-                "cpu_temp": piTemp,
-                "cpu_voltage": piVoltage
-            },
-            lel
-    
-        }
-    
+    if(ReqCounter === 1) {
+
+        var msTakenforTest = fib(30)
+
+        var piTemp
+        var piVoltage;
+
         if(isPi()) {
+            process2.exec("vcgencmd measure_temp", function (err,stdout,stderr) {stdout.split("="); var stdout1 = stdout[1].split("'"); piTemp = stdout1[0]})
+            process2.exec("vcgencmd measure_volts", function (err,stdout,stderr) {stdout.split("="); piVoltage = stdout[1].replace("V", "")})
+        }
+
+
+        si.get({
+            osInfo: "*",
+            system: "*",
+            shell: "*",
+
+            audio: "*",
+
+            baseboard: "*",
+            chassis: "*",
+
+            cpu: "*",
+            //cpuFlags: "*",
+            cpuCache: "*", //in bytes
+            cpuCurrentSpeed: "*",
+            cpuTemperature: "*",
+
+            mem: "*", //in bytes
+            memLayout: "*", //in bytes
+
+            battery: "*",
+
+            graphics: "*",
+            versions: "*",
+            users: "*",
+            FullLoad: "*", //highest CPU % since boot
+
+            processes: "all, running, sleeping, unknown, blocked",
+            
+            //processLoad("process_name, process_name2"): "*", See: https://systeminformation.io/processes.html
+            services: "*",
+
+            //diskLayout: "*",
+            fsOpenFiles: "*",
+            fsStats: "*", //See: https://systeminformation.io/filesystem.html
+            fsSize: "*",
+
+            usb: "*",
+            printer: "*",
+
+            //networkInterfaces: "*",
+            networkInterfaceDefault: "*",
+            networkGatewayDefault: "*",  //router ip: 10.0.0.138
+            networkStats: "*", //si.networkStats(iface) iface= interface to scan
+            //networkConnections: "*",
+            //inetLatency: "*", inetLatency(google.com)
+
+            wifiNetworks: "*",
+            wifiInterfaces: "*",
+            wifiConnections: "*",
+
+            bluetoothDevices: "*",
+
+            dockerInfo: "containers, containersRunning, containersPaused, containersStopped, images, memoryLimit, operatingSystem, memTotal, experimentalBuild, serverVersion",
+            dockerContainers: "*",
+            // dockerImages: "*",
+            //dockerContainerStats: "*",
+
+            vboxInfo: "*",
+        }).then(SIdata => {
+            cpu.usage().then(usage => {Processorusage = usage})
+            var today = new Date();
+            var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+            var data;
+        
             data = {
                 "success" : true,
-                "time" : time,
-                "cpu_usage" : Processorusage,
-                "cpu_core_count" : cpu.count(),
-                "logged_in_user" : username,
-                "RAM" : freeMemory,
-                "cpu_type" : cpuCoresInfo,
-                "storage_info" : diskInfo,
-                "os_version" : osVersion,
+                "time": time,
+                "cpu_usage": Processorusage,
                 "performance_test": msTakenforTest,
-                "Raspi": {
-                    "is_pi": isRaspi,
-                    "cpu_temp": piTemp,
-                    "cpu_voltage": piVoltage
-                },
-                lel
-    
+                SIdata
             }
-        }
-    
-    
-        try {
-            res.send(data)
-            console.log("Request made, content served successfully")
-            console.log("content served successfully")
-            if(process.env.MODE === "debug") {
-                console.log("")
+        
+            if(isPi()) {
+                data = {
+                    "success" : true,
+                    "time" : time,
+                    "cpu_usage": Processorusage,
+                    "performance_test": msTakenforTest,
+                    "Raspi": {
+                        "is_pi": isPi(),
+                        "cpu_temp": piTemp,
+                        "cpu_voltage": piVoltage
+                    },
+                    SIdata
+                }
             }
-        } catch(err) {
-           console.log("An error occurred while responding to an API request: ", err)
-        }
-    }
+        
+        
+            try {
+                res.send(data)
+                console.log("Request made, content served successfully")
+                console.log("content served successfully")
+                if(process.env.MODE === "debug") {
+                    console.log("")
+                }
+            } catch(err) {
+            console.log("An error occurred while responding to an API request: ", err)
+            }
+        });
+    } else res.send("wip")
+    ReqCounter++;
 })
 
 app.listen(process.env.PORT, function () {
 
    console.log("API operating at http://localhost:" + process.env.PORT + "/" + process.env.PATH_AFTER_URL)
 })
+
+await fetch("http://localhost:" + process.env.PORT + "/" + process.env.PATH_AFTER_URL, {
+    headers: {
+        "EucoAPIAuth": "IamRobot"
+    }
+}).then(function (response) {console.log(response)})
