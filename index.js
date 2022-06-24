4590b5b3e659c6e881f9c38654236b2e81597c57
Creative-Difficulty <80907917+Creative-Difficulty@users.noreply.github.com>
2022-06-24 15:03:04 +0200

    add Promise API support for all lib files

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 import * as fs from "fs/promises";
-import * as process2 from "child_process";
-
-import {dist, fib} from "cpu-benchmark";
 
 import checkENV from "./lib/checkENV.js";
 import checkUsersCorruption from "./lib/checkUsersCorruption.js";
 import crypto from 'crypto';
 import express from "express";
 import fetch from "node-fetch";
-import find from 'local-devices';
+import getSystemData from "./lib/getSystemData.js";
 import initLogger from "./lib/initLogger.js";
 import ip from "ip";
-import isPi from "detect-rpi";
 import log4js from 'log4js';
-import osu from "node-os-utils";
 import rateLimit from 'express-rate-limit'
 import si from "systeminformation";
 import slowDown from "express-slow-down"
@@ -34,9 +29,6 @@ log4js.configure(LoggerConfig)
 
 //logger.debug("does this work?");
 
-var Processorusage;
-var DevicesInNetwork;
-
 logger.info("Welcome to EucoAPIv0.1")
 
 
@@ -49,7 +41,7 @@ fs.readFile("users.json", "utf-8", (err, result) => {
     } finally {
         for (let i = 0; i < result.length; i++) {
             for (var name in result[i]) {
-                logger.debug(result[i][name].ip);
+                //logger.debug(result[i][name].ip);
                 if(result[i]["has-access"] === true) {
                     allowedIPs.push(result[i][name].ip);
                 }
@@ -59,7 +51,7 @@ fs.readFile("users.json", "utf-8", (err, result) => {
 })
 
 
-logger.debug(allowedIPs)
+//logger.debug(allowedIPs)
 
 const speedLimiter = slowDown({
     windowMs: 1 * 60 * 1000, // 1 minute
@@ -85,9 +77,9 @@ await si.networkStats()
 var ReqCounter = 0;
 
 
-app.get("/auth", function (req, res) {
+app.get("/auth", async function (req, res) {
     const token = crypto.randomBytes(48).toString('hex');
-    fs.readFile("users.json", "utf-8", (err, data) => {
+    fs.readFile("users.json", "utf-8", async (err, data) => {
         if (err) throw err;
 
         if(!data.includes("[") || !data.includes("]") || data === "" || data === null || data === undefined) throw new Error("Something went wrong");
@@ -100,7 +92,7 @@ app.get("/auth", function (req, res) {
         }
 
         parsedData.push(newUser);
-        fs.writeFile("users.json", JSON.stringify(parsedData), (data, err) => {if (err) throw err;});
+        await fs.writeFile("users.json", JSON.stringify(parsedData), (data, err) => {if (err) throw err;});
     });
     
     res.send({
@@ -108,26 +100,20 @@ app.get("/auth", function (req, res) {
     })
 })
 
-app.get("/" + process.env.URI, function (req, res) {
+app.get("/" + process.env.URI, async (req, res) => {
     
     logger.debug("REQUEST:")
 
     var requestIP = req.headers['x-forwarded-for'] || req.socket.remoteAddress || ''.split(',')[0].trim() || req.socket.localAddress || req.ip
-    if (requestIP.substr(0, 7) == "::ffff:") {
-        requestIP = requestIP.substr(7)
-    }
-
-
-
     var localIP = ip.address()
 
-    if(req.socket.localAddress === requestIP) {
-        logger.debug(`IP: this PCs IP (${localIP})`)
-        logger.debug("LOCATION: Not accessible, the client is in the same network as the server")
+    if(req.socket.localAddress === requestIP || localIP === requestIP) {
+        logger.debug(`  IP: this PCs IP (${localIP})`)
+        logger.debug("  LOCATION: Not accessible, the client is in the same network as the server")
     } else {
-        logger.debug(`IP: ${requestIP}`)
+        logger.debug(`IP: ${readableRequestIP}`)
         var ipLocation
-        var requestURL = "http://ip-api.com/json/" + requestIP
+        var requestURL = "http://ip-api.com/json/" + readableRequestIP
         fetch(requestURL).then(jsonData => jsonData.json()).then(jsonData => {
             ipLocation = jsonData
             logger.debug(ipLocation)
@@ -138,219 +124,18 @@ app.get("/" + process.env.URI, function (req, res) {
             logger.debug("LOCATION:" + ipLocation)
         }
     }
-    
-    
-    req.header("Authentication", "Basic")
-    if(ReqCounter === 0) {
-        var msTakenforTest = fib(30)
 
-        var piTemp
-        var piVoltage;
+    const data = await getSystemData(ReqCounter);
 
-        if(isPi()) {
-            process2.exec("vcgencmd measure_temp", function (err,stdout,stderr) {stdout.split("="); var stdout1 = stdout[1].split("'"); piTemp = stdout1[0]})
-            process2.exec("vcgencmd measure_volts", function (err,stdout,stderr) {stdout.split("="); piVoltage = stdout[1].replace("V", "")})
+    try {
+        res.send(Buffer.from(JSON.stringify(data)).toString("base64"))
+        logger.debug("Request made, content served successfully")
+        logger.debug("content served successfully")
+        if(logger.isDebugEnabled()) {
+            console.log("")
         }
-
-
-        si.get({
-            osInfo: "*",
-            system: "*",
-            shell: "*",
-            bios: "*",
-
-            audio: "*",
-
-            baseboard: "*",
-            chassis: "*",
-
-            cpu: "*",
-            //cpuFlags: "*",
-            cpuCache: "*", //in bytes
-            cpuCurrentSpeed: "*",
-            cpuTemperature: "*",
-
-            mem: "*", //in bytes
-            memLayout: "*", //in bytes
-
-            battery: "*",
-
-            graphics: "*",
-            versions: "*",
-            users: "*",
-            FullLoad: "*", //highest CPU % since boot
-
-            processes: "all, running, sleeping, unknown, blocked",
-            
-            //processLoad("process_name, process_name2"): "*", See: https://systeminformation.io/processes.html
-            services: "*",
-
-            //diskLayout: "*",
-            fsOpenFiles: "*",
-            fsStats: "*", //See: https://systeminformation.io/filesystem.html
-            fsSize: "*",
-
-            usb: "*",
-            printer: "*",
-
-            //networkInterfaces: "*",
-            networkInterfaceDefault: "*",
-            networkGatewayDefault: "*",  //router ip: 10.0.0.138
-            networkStats: "*", //si.networkStats(iface) iface= interface to scan
-            //networkConnections: "*",
-            //inetLatency: "*", inetLatency(google.com)
-
-            wifiNetworks: "*",
-            wifiInterfaces: "*",
-            wifiConnections: "*",
-
-            bluetoothDevices: "*",
-
-            dockerInfo: "containers, containersRunning, containersPaused, containersStopped, images, memoryLimit, operatingSystem, memTotal, experimentalBuild, serverVersion",
-            dockerContainers: "*",
-            // dockerImages: "*",
-            //dockerContainerStats: "*",
-
-            vboxInfo: "*",
-        }).then(SIdata => {
-            osu.cpu.usage().then(data => {Processorusage = data})
-            find().then(data => {DevicesInNetwork = data; console.log(DevicesInNetwork)})
-            var today = new Date();
-            var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        
-            var data = {
-                "s" : true,
-                "tsys": time,
-                "cu": Processorusage,
-                "pt": msTakenforTest,
-                "dism": DevicesInNetwork,
-                SIdata,
-                
-            }
-        
-            if(isPi()) {
-                data = {
-                    "s" : true,
-                    "t": time,
-                    "cu": Processorusage,
-                    "pt": msTakenforTest,
-                    "d": DevicesInNetwork,
-                    "r": {
-                        "ip": isPi(),
-                        "t": piTemp,
-                        "cv": piVoltage
-                    },
-                    SIdata
-                }
-            }
-        
-        
-            try {
-                res.send(Buffer.from(JSON.stringify(data)).toString("base64"))
-                logger.info("Request made, content served successfully")
-                logger.info("content served successfully")
-                if(process.env.LOGLEVEL === "debug") {
-                    console.log("")
-                }
-            } catch(err) {
-                logger.error("An error occurred while responding to an API request: ", err)
-            }
-        });
-    } else {
-        var msTakenforTest = fib(30)
-
-        var piTemp
-        var piVoltage;
-
-        if(isPi()) {
-            process2.exec("vcgencmd measure_temp", function (err,stdout,stderr) {stdout.split("="); var stdout1 = stdout[1].split("'"); piTemp = stdout1[0]})
-            process2.exec("vcgencmd measure_volts", function (err,stdout,stderr) {stdout.split("="); piVoltage = stdout[1].replace("V", "")})
-        }
-
-
-        si.get({
-            cpu: "*",
-            //cpuFlags: "*",
-            cpuCurrentSpeed: "*",
-            cpuTemperature: "*",
-
-            mem: "*", //in bytes
-            memLayout: "*", //in bytes
-
-            battery: "*",
-
-            FullLoad: "*", //highest CPU % since boot
-
-            processes: "all, running, sleeping, unknown, blocked",
-            
-            //processLoad("process_name, process_name2"): "*", See: https://systeminformation.io/processes.html
-            services: "*",
-
-            //diskLayout: "*",
-            fsOpenFiles: "*",
-            fsStats: "*", //See: https://systeminformation.io/filesystem.html
-            fsSize: "*",
-
-            usb: "*",
-            
-            networkStats: "*", //si.networkStats(iface) iface= interface to scan
-            //networkConnections: "*",
-            //inetLatency: "*", inetLatency(google.com)
-
-
-            wifiConnections: "*",
-
-            bluetoothDevices: "*",
-
-            dockerInfo: "containers, containersRunning, containersPaused, containersStopped, images, memoryLimit, operatingSystem, memTotal, experimentalBuild, serverVersion",
-            dockerContainers: "*",
-            // dockerImages: "*",
-            //dockerContainerStats: "*",
-
-            vboxInfo: "*",
-        }).then(SIdata => {
-            find().then(data => {DevicesInNetwork = data;})
-            osu.cpu.usage().then(usage => {Processorusage = usage})
-            var today = new Date();
-            var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-            var data;
-        
-            data = {
-                "s" : true,
-                "t": time,
-                "cu": Processorusage,
-                "pt": msTakenforTest,
-                "d": DevicesInNetwork,
-                SIdata
-            }
-        
-            if(isPi()) {
-                data = {
-                    "success" : true,
-                    "time" : time,
-                    "cpu_usage": Processorusage,
-                    "performance_test": msTakenforTest,
-                    "Raspi": {
-                        "is_pi": isPi(),
-                        "cpu_temp": piTemp,
-                        "cpu_voltage": piVoltage
-                    },
-                    SIdata
-                }
-            }
-        
-        
-            try {
-                res.send(Buffer.from(JSON.stringify(data)).toString("base64"))
-                logger.debug("Request made, content served successfully")
-                logger.debug("content served successfully")
-                if(process.env.LOGLEVEL === "debug") {
-                    console.log("")
-                }
-            } catch(err) {
-                logger.error("An error occurred while responding to an API request: ", err)
-            }
-        });
+    } catch(err) {
+        logger.error("An error occurred while responding to an API request: ", err)
     }
     ReqCounter++;
 })
